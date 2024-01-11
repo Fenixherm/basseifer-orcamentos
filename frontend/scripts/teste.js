5f04caac6c2043d03a4b1fe64522de4071c769d7
Guilherme <guibah78@gmail.com>
2024-01-11 13:01:18 -0300

    lógica dos orçamentos do mes e aprovados, terminar dos planilhadores

--- frontend/scripts/teste.js
+++ frontend/scripts/teste.js
@@ -5,7 +5,14 @@ const usuarios = [
     // ... outros usuários
   ];
 
-  function criarTabelaUsuarios(usuarios) {
+const orcamentos = [
+  { nome: 'João', telefone: '123456789', cep: '12345-678', categoria: 'aprovado', planilhador:'Lucas' },
+    { nome: 'Maria', telefone: '987654321', cep: '54321-098',  categoria: 'aprovado', planilhador:'Maria' 
+   }, {
+    nome: 'Maria', telefone: '987654321', cep: '54321-098',  categoria: '', planilhador:'Pedro'},
+]
+
+  function ultimosOrcamentos(usuarios) {
     const tabela = document.createElement('table');
     const cabecalho = tabela.createTHead();
     const linhaCabecalho = cabecalho.insertRow();
@@ -33,5 +40,54 @@ const usuarios = [
     document.getElementById('ultimos-orcamentos').appendChild(tabela);
   }
   
+  function orcamentosDoMes(orcamentos){
+    const qntdOrcamentos = document.querySelector('.qntdOrcamentos');
+    const orcamentosAprovados = document.querySelector('.orcamentos-aprovados');
+    let aprovados = 0;
+
+    if (qntdOrcamentos) {
+      qntdOrcamentos.textContent = orcamentos.length;
+      
+      for(let i in orcamentos){
+        if(orcamentos[i].categoria.toLowerCase() === 'aprovado'){
+          aprovados++;
+        }
+      }
+      orcamentosAprovados.textContent = aprovados;
+    }
+  }
+
+  function orcamentoPorPlanilhador(orcamentos){
+    const tabela = document.createElement('table');
+    const cabecalho = tabela.createTHead();
+    const linhaCabecalho = cabecalho.insertRow();
+  
+    // Cabeçalho da tabela
+    for (let chave in orcamentos[0]) {
+      const th = document.createElement('th');
+      const texto = document.createTextNode(chave.charAt(0).toUpperCase() + chave.slice(1));
+      th.appendChild(texto);
+      linhaCabecalho.appendChild(th);
+    }
+  
+    // Linhas com os dados dos usuários
+    const corpoTabela = tabela.createTBody();
+    orcamentos.forEach((orcamentos) => {
+      const linha = corpoTabela.insertRow();
+      for (let chave in orcamentos) {
+        const cell = linha.insertCell();
+        const texto = document.createTextNode(orcamentos[chave]);
+        cell.appendChild(texto);
+      }
+    });
+  
+    // Adiciona a tabela ao elemento com id "tabela-usuarios"
+    document.getElementById('por-planilhador').appendChild(tabela);
+  }
+
+
+
   // Chama a função para criar a tabela com os usuários
-  criarTabelaUsuarios(usuarios);
+  ultimosOrcamentos(usuarios);
+  orcamentosDoMes(orcamentos);
+  orcamentoPorPlanilhador(orcamentos);
